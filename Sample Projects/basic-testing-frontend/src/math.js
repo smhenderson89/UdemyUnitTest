20c0e03cc26e0dffde613805acf99df302e0d4fc
Scott Henderson <scott.mc.henderson@gmail.com>
2023-05-20 21:15:41 -0700

    class progress 23.05.20

--- Sample Projects/basic-testing-frontend/src/math.js
+++ Sample Projects/basic-testing-frontend/src/math.js
@@ -1,3 +1,5 @@
+import { cleanNumbers } from "./util/numbers.js";
+
 export function add(numbers) {
   let sum = 0;
 
@@ -5,6 +7,21 @@ export function add(numbers) {
 
   for (const number of numbers) {
     sum += +number; // "+" -> convert string to array
+    // console.log(`Add Numbers: ${sum}`);
   }
   return sum;
+}
+
+export function calculateResult(numberValues) {
+  // valdiate input
+  let result = '';
+  try {
+    const numbers = cleanNumbers(numberValues);
+    // derive a result if result succeded or failed
+    result = add(numbers).toString();
+  } catch (error) {
+    result = error.message;
+  }
+  // console.log(`Calculated result: ${result}`);
+  return result;
 }
--- Sample Projects/basic-testing-frontend/src/parser.js
+++ Sample Projects/basic-testing-frontend/src/parser.js
@@ -1,7 +1,13 @@
 export function extractNumbers(formData) {
   const num1Input = formData.get('num1');
   const num2Input = formData.get('num2');
+  // console.log(`#1: ${num1Input}, #2: ${num2Input}`)
 
   return [num1Input, num2Input];
 }
 
+export function extractEnteredNumberValues(form) {
+  const formData = new FormData(form);
+  const numberInputs = extractNumbers(formData);
+  return numberInputs;
+}
